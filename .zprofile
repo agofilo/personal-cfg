26819a8a205fd44c37e7dcb461c3fe66ffec0bdc
Luke VanderHart <luke@thinkrelevance.com>
2011-02-16 12:38:42 -0500

    Added Aaron's zsh config

--- .zprofile
+++ .zprofile
@@ -7,9 +7,6 @@ PROMPT=%~'>'
 
 PATH=/Users/luke/bin:$PATH
 
-source ~/relevance/etc/bash/git_autocompletion.sh
-source ~/relevance/etc/bash/git_prompt.sh
-
 PROMPT=$PS1
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
---
+++ .zsh/aliases.zsh
@@ -0,0 +1,15 @@
+alias port='sudo port -v'
+alias ssh='/usr/bin/ssh'
+
+alias ls='ls -GFh'
+alias ll='ls -GFhl'
+
+alias ri='ri -Tf ansi'
+alias rdoc='rdoc --ri --line-numbers --all'
+
+alias vi='vim'
+alias vim='vim'
+alias gvim='mvim -p'
+alias e='open -a Aquamacs'
+
+alias cycle_passenger='touch tmp/restart.txt'
