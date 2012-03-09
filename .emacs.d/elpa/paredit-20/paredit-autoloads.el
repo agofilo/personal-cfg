6235f15b884002c371895be34e97f55733b36ef6
Luke VanderHart <luke@thinkrelevance.com>
2012-03-09 10:22:28 -0500

    deleted old emacs cfg

--- .emacs.d/elpa/paredit-20/paredit-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; paredit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (paredit-mode) "paredit" "../../../../../.emacs.d/elpa/paredit-20/paredit.el"
-;;;;;;  (20120 16078))
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/paredit-20/paredit.el
-
-(autoload 'paredit-mode "paredit" "\
-Minor mode for pseudo-structurally editing Lisp code.
-\\<paredit-mode-map>
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/paredit-20/paredit-pkg.el"
-;;;;;;  "../../../../../.emacs.d/elpa/paredit-20/paredit.el") (20120
-;;;;;;  16078 591218))
-
-;;;***
-
-(provide 'paredit-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; paredit-autoloads.el ends here
